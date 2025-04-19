e4d017da238730675d03708f0e6cb85bdb01999e
fbado <reginaspeedboat@gmail.com>
2025-04-19 13:56:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RedSeaBoatRental from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RedSeaBoatRental />
+  </React.StrictMode>
+);
